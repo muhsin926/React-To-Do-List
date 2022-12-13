efd4351cf1683dc64be21ed72800c00c9d1c332a
muhsin926 <muhsinmp926@gmail.com>
2022-12-13 16:19:06 +0530

    create and checked

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [toDos, setToDos] = useState([])
+  const [toDo, setToDo] = useState('')
   return (
     <div className="app">
       <div className="mainHeading">
@@ -12,19 +15,40 @@ function App() {
         <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <input value={toDo} onChange={(e) => setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={() => setToDos([...toDos, { id: Date.now(), text: toDo, status: false }])} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-        <div className="todo">
-          <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
-          </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
-          </div>
-        </div>
+        {
+          toDos.map((value) => {
+            return (
+              <div className="todo">
+                <div className="left">
+                  <input onChange={(e) => {
+                    setToDos(toDos.filter(obj => {
+                      if (obj.id == value.id) {
+                        obj.status = e.target.checked
+                      }
+                      return obj
+                    }))
+                  }} value={value.status} type="checkbox" name="" id="" />
+                  <p>{value.text}</p>
+                </div>
+                <div className="right">
+                  <i className="fas fa-times"></i>
+                </div>
+              </div>
+            )
+          })
+        }
+        {
+          toDos.map((obj) => {
+            if (obj.status) {
+              return (<h1>{obj.text}</h1>)
+            }
+            return null
+          })
+        }
       </div>
     </div>
   );
